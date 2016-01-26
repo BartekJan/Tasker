e541af3adb68172a6153f8c5a634df3b91fdec79
James Downing <jamesdowning15@gmail.com>
2016-01-26 23:00:42 +0000

    Tidied task window GUI.

--- Tasker/src/TaskManTasks.java
+++ Tasker/src/TaskManTasks.java
@@ -10,8 +10,10 @@ import java.awt.Color;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.SwingConstants;
 import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 
@@ -48,7 +50,7 @@ public class TaskManTasks {
 	private void initialize() {
 		frmTasks = new JFrame();
 		frmTasks.setTitle("Tasks");
-		frmTasks.setBounds(100, 100, 612, 456);
+		frmTasks.setBounds(100, 100, 700, 500);
 		frmTasks.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmTasks.getContentPane().setLayout(null);
 		
@@ -68,32 +70,43 @@ public class TaskManTasks {
 		frmTasks.getContentPane().add(taskList);
 		
 		JButton updateButton = new JButton("Update");
-		updateButton.setBounds(341, 26, 89, 23);
+		updateButton.setBounds(500, 15, 89, 23);
 		frmTasks.getContentPane().add(updateButton);
 		
 		JButton logoutButton = new JButton("Log out");
-		logoutButton.setBounds(473, 26, 89, 23);
+		logoutButton.setBounds(600, 15, 89, 23);
 		frmTasks.getContentPane().add(logoutButton);
 		
-		JLabel progressBarTitle = new JLabel("Progress Bar");
+		JLabel progressBarTitle = new JLabel("Progress Bar:");
 		progressBarTitle.setHorizontalAlignment(SwingConstants.CENTER);
-		progressBarTitle.setBounds(241, 269, 149, 23);
+		progressBarTitle.setBounds(250, 420, 149, 23);
 		frmTasks.getContentPane().add(progressBarTitle);
 		
 		JProgressBar progressBar = new JProgressBar();
-		progressBar.setBounds(241, 294, 149, 23);
+		progressBar.setBounds(250, 440, 149, 23);
 		frmTasks.getContentPane().add(progressBar);
 		
 		JTextArea taskContent = new JTextArea();
-		taskContent.setBounds(216, 145, 200, 113);
+		taskContent.setBounds(200, 240, 300, 150);
 		frmTasks.getContentPane().add(taskContent);
 		
 		JLabel lblTaskContent = new JLabel("Task content:");
-		lblTaskContent.setBounds(279, 120, 83, 14);
+		lblTaskContent.setBounds(210, 60, 100, 14);
 		frmTasks.getContentPane().add(lblTaskContent);
 		
+		/* Can set this to take text from a URL, was thinking this was a way to 
+		display the task details?*/
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setEditable(false);
+		editorPane.setBounds(200, 80, 300, 150);
+		//Enable scroll bar.
+		JScrollPane editorScrollPane = new JScrollPane(editorPane);
+		editorScrollPane.setVerticalScrollBarPolicy(
+		                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		frmTasks.getContentPane().add(editorPane);
+		
 		JButton commentButton = new JButton("Add comment");
-		commentButton.setBounds(251, 338, 125, 23);
+		commentButton.setBounds(270, 390, 150, 23);
 		frmTasks.getContentPane().add(commentButton);
 	}
 
@@ -103,102 +116,3 @@ public class TaskManTasks {
 	}
 }
 
-
-/*import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-
-public class TaskManTasks extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-/*	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TaskManTasks frame = new TaskManTasks();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-/*	public TaskManTasks() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 612, 456);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel userPic = new JLabel("User pic");
-		Image defaultUserPic= new ImageIcon(this.getClass().getResource("/DefaultUser2.png")).getImage();
-		userPic.setIcon(new ImageIcon(defaultUserPic));
-		userPic.setBounds(29, 30, 72, 72);
-		contentPane.getRootPane().add(userPic);
-		
-		JLabel userName = new JLabel("User name");
-		userName.setBounds(136, 26, 149, 23);
-		contentPane.getRootPane().add(userName);
-		
-		JList taskList = new JList();
-		taskList.setBorder(new LineBorder(new Color(0, 0, 0)));
-		taskList.setBounds(136, 338, -109, -192);
-		contentPane.getRootPane().add(taskList);
-		
-		JButton updateButton = new JButton("Update");
-		updateButton.setBounds(341, 26, 89, 23);
-		contentPane.getRootPane().add(updateButton);
-		
-		JButton logoutButton = new JButton("Log out");
-		logoutButton.setBounds(473, 26, 89, 23);
-		contentPane.getRootPane().add(logoutButton);
-		
-		JLabel progressBarTitle = new JLabel("Progress Bar");
-		progressBarTitle.setHorizontalAlignment(SwingConstants.CENTER);
-		progressBarTitle.setBounds(241, 269, 149, 23);
-		contentPane.getRootPane().add(progressBarTitle);
-		
-		JProgressBar progressBar = new JProgressBar();
-		progressBar.setBounds(241, 294, 149, 23);
-		contentPane.getRootPane().add(progressBar);
-		
-		JTextArea taskContent = new JTextArea();
-		taskContent.setBounds(216, 145, 200, 113);
-		contentPane.getRootPane().add(taskContent);
-		
-		JLabel lblTaskContent = new JLabel("Task content:");
-		lblTaskContent.setBounds(279, 120, 83, 14);
-		contentPane.getRootPane().add(lblTaskContent);
-		
-		JButton commentButton = new JButton("Add comment");
-		commentButton.setBounds(251, 338, 125, 23);
-		contentPane.getRootPane().add(commentButton);
-	}
-}
-*/
